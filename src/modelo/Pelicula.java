97b33b188a1f5db2452fc2bb77a4d1d84021069a
Aulas Informáticas Universidad El Bosque <aulainformatica@unbosque.edu.co>
2025-05-12 19:11:50 -0500

    clase pelicula inicial

--- src/modelo/Pelicula.java
+++ src/modelo/Pelicula.java
@@ -5,4 +5,18 @@ import java.time.LocalDate;
 public class Pelicula {
 private String nombre;
 private LocalDate fechaEstreno;
+private int idPelicula;
+private String genero;
+private int calificacion;
+public double duracionEnHoras;
+
+public Pelicula(String nombre, LocalDate fechaEstreno, int idPelicula, String genero, int calificacion, double duracionEnHoras) {
+	this.nombre=nombre;
+	this.fechaEstreno=fechaEstreno;
+	this.idPelicula=idPelicula;
+	this.genero=genero;
+	this.calificacion=calificacion;
+	this.duracionEnHoras=duracionEnHoras;
+}
+
 }
