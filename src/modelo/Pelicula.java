b978c2f7a31e028775640201dc2d52636169f13a
Aulas Informáticas Universidad El Bosque <aulainformatica@unbosque.edu.co>
2025-05-12 19:45:17 -0500

    geters

--- src/modelo/Pelicula.java
+++ src/modelo/Pelicula.java
@@ -4,6 +4,54 @@ import java.time.LocalDate;
 
 public class Pelicula {
 private String nombre;
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+public LocalDate getFechaEstreno() {
+	return fechaEstreno;
+}
+
+public void setFechaEstreno(LocalDate fechaEstreno) {
+	this.fechaEstreno = fechaEstreno;
+}
+
+public int getIdPelicula() {
+	return idPelicula;
+}
+
+public void setIdPelicula(int idPelicula) {
+	this.idPelicula = idPelicula;
+}
+
+public String getGenero() {
+	return genero;
+}
+
+public void setGenero(String genero) {
+	this.genero = genero;
+}
+
+public int getCalificacion() {
+	return calificacion;
+}
+
+public void setCalificacion(int calificacion) {
+	this.calificacion = calificacion;
+}
+
+public double getDuracionEnHoras() {
+	return duracionEnHoras;
+}
+
+public void setDuracionEnHoras(double duracionEnHoras) {
+	this.duracionEnHoras = duracionEnHoras;
+}
+
 private LocalDate fechaEstreno;
 private int idPelicula;
 private String genero;
