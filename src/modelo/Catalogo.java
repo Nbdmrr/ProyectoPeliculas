0fe52bb7f626616200c8b98df1da7347253aa39e
mrodriguezav <mrodriguezav@unbosque.edu.co>
2025-05-12 19:17:57 -0500

    clase catalogo

--- src/modelo/Catalogo.java
+++ src/modelo/Catalogo.java
@@ -1,5 +1,39 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Catalogo {
+	private ArrayList<Pelicula> catalogoPeliculas;
+	private double duracion;
+	
+	public Catalogo() {
+	catalogoPeliculas= new ArrayList<Pelicula>();
+	
+		
+		duracion = calcularHorasCatalogo();
+		
+		
+	}
+	
+	
+	
+	public double calcularHorasCatalogo() {
+		
+		double horasCatalogo = 0;
+		
+		for (Pelicula pelicula : catalogoPeliculas) {
+			
+			double horasPelicula = pelicula.duracionEnHoras;
+			
+			horasCatalogo+=horasPelicula;
+			
+			
+		}
+		return horasCatalogo;
+		
+		
+	}
+	
+	
 
 }
